13835fcd581561650d39f463e1c2051551ea768b
hyunsung1094 <hyunsung109@gmail.com>
2021-03-23 14:44:50 +0900

    12번 문제 소스코드 추가

--- main.cpp
+++ main.cpp
@@ -3,17 +3,24 @@
 #include <iostream>
 using namespace std;
 
-void func1()
+int func12()
 {
-	int a = 0, b = 0;
+    int a1 = 0, b1 = 0;
 	cout << "두 개의 숫자를 입력하세요 : ";
-	cin >> a >> b;
+	cin >> a1 >> b1;
 
-	int temp = a;
-	a = b;
-	b = temp;
+    int a2 = a1;
+    int b2 = b1;
 
-	cout << "a : " << a << ", " << "b : " << b << endl;
+    while(b1 != 0) {
+        int c = a1 % b1;
+        a1 = b1;
+        b1 = c;
+    }
+    int gcd = a1;
+    int lcm = a2 * b2 / gcd;
+
+    cout << "LCM : " << lcm << endl;
 }
 
 int main()
@@ -41,7 +48,7 @@ int main()
 		cout << "Select Number>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
 		cin >> nInputManu;
 
-		if(nInputManu==1) func1();
+		if(nInputManu==1) { }
 		else if(nInputManu==2) { }
 		else if(nInputManu==3) { }
 		else if(nInputManu==4) { }
@@ -52,7 +59,7 @@ int main()
 		else if(nInputManu==9) { }
 		else if(nInputManu==10) { }
 		else if(nInputManu==11) { }
-		else if(nInputManu==12) { }
+		else if(nInputManu==12) func12();
 		else break;
 	}
 
